90a92d883fc6bfcf533339bd0552c1a82752d8e6
EdsonAleman910 <edsnalemancu@gmail.com>
2024-10-30 19:52:58 -0600

    Se añadio practica corrutinas

--- app/src/main/java/com/example/loto/viewModels/LoteriaViewModels.kt
+++ app/src/main/java/com/example/loto/viewModels/LoteriaViewModels.kt
@@ -34,23 +34,23 @@ class LoteriaViewModels : ViewModel() {
         }
     }
 
-    //Este ya no se usa
+
     fun generateLotoNumbers(){
         _lotoNumbers.value = (1..60).shuffled().take(6).sorted()
     }
 
-   //Funcion para generar la lista un elemento a la vez
+
    private suspend fun generarLista() {
 
-       val generatedNumbers = mutableListOf<Int>() //Crea una lista Mutable
+       val generatedNumbers = mutableListOf<Int>()
        for (i in 1..6) {
-           val number = (1..60).shuffled().first() //Se guarda en la variable number
-           generatedNumbers.add(number)             //metodo para añadir a la lista el numero generado
-           _lotoNumbers.value = generatedNumbers.toList().sorted() //Metodo para guardar en _lotoNumbers la lista ordenada
-           delay(2000) //delay de 2 segundos
-           bolita++ //aumenta por uno la varible bolita que solo muestra el numero de numeros que se han generado
+           val number = (1..60).shuffled().first()
+           generatedNumbers.add(number)
+           _lotoNumbers.value = generatedNumbers.toList().sorted()
+           delay(2000)
+           bolita++
        }
-       bolita = 1 //regresa a 1 la variable para que la proxima vez que se mande a llamar la función comience en 1
+       bolita = 1
    }
 
 
